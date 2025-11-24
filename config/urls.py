f48dab22960ba7de0601e290697d4ecee85b55b3
Olga Noskova <olga.tolba@gmail.com>
2025-11-24 22:04:48 +1000

    Refactor project after auth and recipient CRUD setup

--- config/urls.py
+++ config/urls.py
@@ -1,10 +1,13 @@
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include
 from django.conf import settings
 from django.conf.urls.static import static
 
 urlpatterns = [
     path("admin/", admin.site.urls),
+    path("", include("mailing.urls")),
+    path("accounts/", include("django.contrib.auth.urls")),
+    path("accounts/", include("users.urls")),
 ]
 
 if settings.DEBUG:
