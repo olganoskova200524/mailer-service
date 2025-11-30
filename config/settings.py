78c94b56983c7d53f885ac990c4fcb3e1fe4870c
Olga Noskova <olga.tolba@gmail.com>
2025-11-30 20:18:59 +1000

    Format mailing dates and localize attempt statuses

--- config/settings.py
+++ config/settings.py
@@ -94,12 +94,12 @@ AUTH_PASSWORD_VALIDATORS = [
     },
 ]
 
-LANGUAGE_CODE = 'en-us'
+LANGUAGE_CODE = 'ru-ru'
 
-TIME_ZONE = 'UTC'
+TIME_ZONE = 'Europe/Moscow'
 
 USE_I18N = True
-
+USE_L10N = True
 USE_TZ = True
 
 STATIC_URL = 'static/'
